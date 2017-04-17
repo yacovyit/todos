d1d126bfbf9931893aba3aa4058a1aa378ecb6ce
Yacov Yitzhak <yacov.mobile@gmail.com>
2017-04-17 10:36:15 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,15 @@ app.post('/todos',(req,res)=>{
     })
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});       
+    },
+    (e)=>{
+        res.status(400).send(e);
+    })
+})
+
 //listen
 app.listen('3000',()=>{
     console.log('Started on port 3000');
