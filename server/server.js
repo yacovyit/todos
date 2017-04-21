ec201ebc994edf4af32e14524aa4f2a181f35db2
Yacov Yitzhak <yacov.mobile@gmail.com>
2017-04-21 07:34:19 +0300

    setup seperate test/development/prod envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+
+require('./config/config');
 const _ = require('lodash');
 
 const express = require('express');
@@ -10,7 +12,7 @@ const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 //crud api
 app.use(bodyParser.json());
 
